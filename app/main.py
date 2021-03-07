56d9fe8d3b19f39b599726f54f9f4bbb3dc781a1
Scott Causby <scottcausby@gmail.com>
2021-03-07 12:43:49 +1100

    fifth

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a path: "/" is the "root"
 @app.route('/')
 def home():
-  return render_template("../templates/index.html")
+  return render_template("index.html")
 
 # Run the app when the program starts!
 if __name__ == '__main__':
